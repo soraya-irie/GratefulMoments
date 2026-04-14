579a6f8006c42e291c05ac750ca6f265c12bf3bd
soraya-ire <rujiangkongshi@gmail.com>
2026-04-14 20:27:57 +0900

    Complete Section2 Step2 - collect, model, and store data

--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -2,7 +2,16 @@ import SwiftUI
 
 struct MomentEntryView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        contentStack
+    }
+
+    var contentStack: some View {
+        VStack(alignment: .leading) {
+            Text("Title")
+
+            Text("Note")
+        }
+        .padding()
     }
 }
 
