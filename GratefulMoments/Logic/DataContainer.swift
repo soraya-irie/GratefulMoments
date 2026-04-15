d1aaf7a25cd19e7e577f24225003359424ff421a
soraya-ire <rujiangkongshi@gmail.com>
2026-04-15 15:02:47 +0900

    Complete Section4 Step6 - collect, model, and store data

--- GratefulMoments/Logic/DataContainer.swift
+++ GratefulMoments/Logic/DataContainer.swift
@@ -8,6 +8,18 @@ class DataContainer {
     }
 
     init() {
+        let schema = Schema([
+            Moment.self,
+        ])
 
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+
+            try context.save()
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
     }
 }
