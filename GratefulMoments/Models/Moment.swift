379129cf6f299fe5b25ba4966e29b3cd4c83b659
soraya-ire <rujiangkongshi@gmail.com>
2026-04-15 12:17:57 +0900

    Complete Section4 Step2 - collect, model, and store data

--- GratefulMoments/Models/Moment.swift
+++ GratefulMoments/Models/Moment.swift
@@ -1,8 +1,17 @@
 import Foundation
+import SwiftData
 
+@Model
 class Moment {
     var title: String
     var note: String
     var imageData: Data?
     var timestamp: Date
+
+    init(title: String, note: String, imageData: Data? = nil, timestamp: Date = .now) {
+        self.title = title
+        self.note = note
+        self.imageData = imageData
+        self.timestamp = timestamp
+    }
 }
