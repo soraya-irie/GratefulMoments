9bebc6899d6d6deda9362d9736a14177a09c06c5
soraya-ire <rujiangkongshi@gmail.com>
2026-04-16 10:09:49 +0900

    Complete Section4 Step2 - use a custom layout view

--- GratefulMoments/Tabs/Moments/MomentHexagonView.swift
+++ GratefulMoments/Tabs/Moments/MomentHexagonView.swift
@@ -1,11 +1,21 @@
 import SwiftUI
 
 struct MomentHexagonView: View {
+    var moment: Moment
+    @State var layout: HexagonLayout = .standard
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Hexagon(layout: layout, moment: moment) {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
 #Preview {
-    MomentHexagonView()
+    ScrollView {
+            MomentHexagonView(moment: Moment.imageSample)
+            MomentHexagonView(moment: Moment.imageSample, layout: .large)
+            MomentHexagonView(moment: Moment.sample)
+            MomentHexagonView(moment: Moment.sample, layout: .large)
+        }
 }
