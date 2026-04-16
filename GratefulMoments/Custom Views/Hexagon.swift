fbce37b38b9a36cf82d23c785866f381642ed626
soraya-ire <rujiangkongshi@gmail.com>
2026-04-16 09:54:28 +0900

    Complete Section3 Step9 - use a custom layout view

--- GratefulMoments/Custom Views/Hexagon.swift
+++ GratefulMoments/Custom Views/Hexagon.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct Hexagon<Content: View>: View {
     private let borderWidth = 2.0
     var borderColor: Color = .ember
-    var size: CGFloat = 350
+    var layout: HexagonLayout = .standard
     var moment: Moment? = nil
     @ViewBuilder var content: () -> Content
 
@@ -16,22 +16,22 @@ struct Hexagon<Content: View>: View {
             }
 
             content()
-                .frame(width: size, height: size)
+                .frame(width: layout.size, height: layout.size)
         }
         .mask {
             Image(systemName: "hexagon.fill")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: size - borderWidth, height: size - borderWidth)
+                .frame(width: layout.size - borderWidth, height: layout.size - borderWidth)
         }
         .background {
             Image(systemName: "hexagon")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: size, height: size)
+                .frame(width: layout.size, height: layout.size)
                 .foregroundStyle(borderColor)
         }
-        .frame(width: size, height: size)
+        .frame(width: layout.size, height: layout.size)
     }
 }
 
