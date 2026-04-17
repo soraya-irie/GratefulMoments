01baa2da8453a622d8cee938328b3f46b4c58417
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 20:57:25 +0900

    Complete Section8 Step1 - create an algorithm for badges

---
+++ GratefulMoments/Tabs/Achievements/StreakView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct StreakView: View {
+    var numberOfDays: Int
+
+    var body: some View {
+        Hexagon(borderColor: .secondary) {
+
+        }
+    }
+}
+
+#Preview {
+    StreakView(numberOfDays: 23)
+}
