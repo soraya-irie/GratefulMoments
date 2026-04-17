b3422bff8b5415c6eaf7cbb33fcce25802b59a5a
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 15:16:29 +0900

    Complete Section5 Step6 - create an algorithm for badges

--- GratefulMoments/Tabs/Achievements/AchievementsView.swift
+++ GratefulMoments/Tabs/Achievements/AchievementsView.swift
@@ -20,7 +20,13 @@ struct AchievementsView: View {
     private var contentStack: some View {
         VStack {
             Text("Your Badges")
+            ForEach(unlockedBadges) { badge in
+                Text(badge.details.title)
+            }
             Text("Locked Badges")
+            ForEach(lockedBadges) { badge in
+                Text(badge.details.title)
+            }
         }
         .padding()
         .frame(maxWidth: .infinity)
