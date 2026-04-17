3980142cf1491bdd9532b88b2c25881be4952afc
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 10:55:14 +0900

    Complete Section1 Step5 - create an algorithm for badges

--- GratefulMoments/Models/BadgeDetails.swift
+++ GratefulMoments/Models/BadgeDetails.swift
@@ -1,5 +1,24 @@
 import Foundation
 
 enum BadgeDetails {
+    case firstEntry
+    case fiveStars
+    case shutterbug
+    case expressive
+    case perfectTen
 
+    var requirements: String {
+        switch self {
+        case .firstEntry:
+            return "Log a moment to start your journey."
+        case .fiveStars:
+            return "Record five moments."
+        case .shutterbug:
+            return "Add three entries with photos."
+        case .expressive:
+            return "Add five moments with a photo and text."
+        case .perfectTen:
+            return "Record at least 10 moments, collecting all the other badges along the way."
+        }
+    }
 }
