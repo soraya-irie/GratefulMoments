b2e01ce584d03586ea9f6353f58a5556b96f4c62
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 21:13:16 +0900

    Complete Section8 Step5 - create an algorithm for badges

---
+++ GratefulMoments/Logic/StreakCalculator.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct StreakCalculator {
+    let calendar = Calendar.current
+
+    /// Counts the number of days in a row a moment has been saved
+    func calculateStreak(for moments: [Moment]) -> Int {
+        return 0
+    }
+}
