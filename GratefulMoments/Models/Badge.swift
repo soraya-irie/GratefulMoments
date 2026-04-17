a48baf891839d4b2ace7bcca5bd8867e3ba72ab2
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 11:48:34 +0900

    Complete Section1 Step13 - create an algorithm for badges

--- GratefulMoments/Models/Badge.swift
+++ GratefulMoments/Models/Badge.swift
@@ -9,3 +9,10 @@ class Badge {
         self.details = details
     }
 }
+
+extension Badge {
+    static var sample: Badge {
+        let badge = Badge(details: .firstEntry)
+        return badge
+    }
+}
