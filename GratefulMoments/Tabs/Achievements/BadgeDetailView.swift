2342a58f2277da49c07fdb117dbaf1e917499d92
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 11:52:22 +0900

    Complete Section2 Step1 - create an algorithm for badges

---
+++ GratefulMoments/Tabs/Achievements/BadgeDetailView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct BadgeDetailView: View {
+    var badge: Badge
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeDetailView(badge: .sample)
+}
