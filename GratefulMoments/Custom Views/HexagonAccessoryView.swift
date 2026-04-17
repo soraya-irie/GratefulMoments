0d8f5321ada68ae7cee08cf521ffe8894f36d4ee
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 19:56:53 +0900

    Complete Section7 Step4 - create an algorithm for badges

--- GratefulMoments/Custom Views/HexagonAccessoryView.swift
+++ GratefulMoments/Custom Views/HexagonAccessoryView.swift
@@ -7,6 +7,10 @@ struct HexagonAccessoryView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+
+    private var size: CGFloat {
+        hexagonLayout.size / 5
+    }
 }
 
 #Preview {
