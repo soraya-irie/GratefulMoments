09dc03ee8dedc2b3e40107690845eff31986a5a7
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 20:18:44 +0900

    Complete Section7 Step6 - create an algorithm for badges

--- GratefulMoments/Custom Views/HexagonAccessoryView.swift
+++ GratefulMoments/Custom Views/HexagonAccessoryView.swift
@@ -5,12 +5,22 @@ struct HexagonAccessoryView: View {
     let hexagonLayout: HexagonLayout
 
     var body: some View {
-        if let badge = badges.first {
-            Image(badge.details.image)
-                .resizable()
-                .frame(width: size, height: size)
-                .shadow(radius: 2)
+        Group {
+            if let badge = badges.first {
+                Image(badge.details.image)
+                    .resizable()
+                    .frame(width: size, height: size)
+                    .shadow(radius: 2)
+            }
         }
+        .offset(y: yOffset)
+    }
+
+    private var yOffset: CGFloat {
+        let radius = hexagonLayout.size / 2
+        // 30 degrees points to the top right corner of a hexagon
+        let yOffsetFromHexagonCenter = sin(Angle.degrees(30).radians) * radius
+        return radius - yOffsetFromHexagonCenter - (size / 2)
     }
 
     private var badges: [Badge] {
