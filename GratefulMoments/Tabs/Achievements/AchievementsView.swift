8b8b462d6d4f9eefe0dcf7686c86433d0899a120
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 15:12:53 +0900

    Complete Section5 Step5 - create an algorithm for badges

--- GratefulMoments/Tabs/Achievements/AchievementsView.swift
+++ GratefulMoments/Tabs/Achievements/AchievementsView.swift
@@ -1,6 +1,13 @@
 import SwiftUI
+import SwiftData
 
 struct AchievementsView: View {
+    @Query(filter: #Predicate<Badge> { $0.timestamp != nil })
+    private var unlockedBadges: [Badge]
+
+    @Query(filter: #Predicate<Badge> { $0.timestamp == nil })
+    private var lockedBadges: [Badge]
+
     var body: some View {
         NavigationStack {
             ScrollView {
