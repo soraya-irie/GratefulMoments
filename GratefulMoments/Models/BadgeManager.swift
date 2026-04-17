af05952339cb68a1185b13ebd80801ede5eba7b6
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 13:43:32 +0900

    Complete Section4 Step6 - create an algorithm for badges

--- GratefulMoments/Models/BadgeManager.swift
+++ GratefulMoments/Models/BadgeManager.swift
@@ -12,6 +12,11 @@ class BadgeManager {
         let context = modelContainer.mainContext
         let moments = try context.fetch(FetchDescriptor<Moment>())
         let lockedBadges = try context.fetch(FetchDescriptor<Badge>(predicate: #Predicate { $0.timestamp == nil}))
+
+        var newlyUnlocked: [Badge] = []
+        for badge in lockedBadges {
+            // TODO: Add badge to newlyUnlocked if earned
+        }
     }
 
     func loadBadgeifNeeded() throws {
