7eabdfc5554d4d509d0bba2c4a51c3cfc648c068
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 21:23:11 +0900

    Complete Section8 Step6 - create an algorithm for badges

--- GratefulMoments/Logic/StreakCalculator.swift
+++ GratefulMoments/Logic/StreakCalculator.swift
@@ -4,7 +4,12 @@ struct StreakCalculator {
     let calendar = Calendar.current
 
     /// Counts the number of days in a row a moment has been saved
+    ///
+    /// Days are measured from the end of the day, rather than whatever time of day it is currently
     func calculateStreak(for moments: [Moment]) -> Int {
+        let startOfToday = calendar.startOfDay(for: .now)
+        let endOfToday = calendar.date(byAdding: DateComponents(day: 1, second: -1), to: startOfToday)!
+
         return 0
     }
 }
