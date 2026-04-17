a68e2829277b803935aef9b5ed6a06ba00acafd4
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 11:09:32 +0900

    Complete Section1 Step6 - create an algorithm for badges

--- GratefulMoments/Models/BadgeDetails.swift
+++ GratefulMoments/Models/BadgeDetails.swift
@@ -21,4 +21,19 @@ enum BadgeDetails {
             return "Record at least 10 moments, collecting all the other badges along the way."
         }
     }
+
+    var congratulatoryMessage: String {
+        switch self {
+        case .firstEntry:
+            return "Every journey begins with a single step. Congratulations - you're on your way!"
+        case .fiveStars:
+            return "You're building momentum! The more you focus on regular practice, the better you get at choosing to keep up intentioned habits."
+        case .shutterbug:
+            return "Photos connect us to our past, and looking at them can take us right back to the grateful feeling we had when we snapped them."
+        case .expressive:
+            return "Look at you, giving yourself all the ways to savor your happy memories!"
+        case .perfectTen:
+            return "You're getting the hang of a new habit! Keep it up and see how far it can take you."
+        }
+    }
 }
