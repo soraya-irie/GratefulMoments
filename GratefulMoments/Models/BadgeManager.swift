669a65290c58f7498c7457e450c2a4011ba1d095
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 14:23:38 +0900

    Complete Section4 Step10 - create an algorithm for badges

--- GratefulMoments/Models/BadgeManager.swift
+++ GratefulMoments/Models/BadgeManager.swift
@@ -18,7 +18,8 @@ class BadgeManager {
             switch badge.details {
             case .firstEntry where moments.count >= 1,
                     .fiveStars where moments.count >= 5,
-                    .shutterbug where moments.count(where: { $0.image != nil }) >= 3:
+                    .shutterbug where moments.count(where: { $0.image != nil }) >= 3,
+                    .expressive where moments.count(where: { $0.image != nil && !$0.note.isEmpty }) >= 5:
                 newlyUnlocked.append(badge)
             default:
                 continue
