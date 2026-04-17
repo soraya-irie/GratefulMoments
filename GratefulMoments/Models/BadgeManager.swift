a78e27754438e05a34f8005c5010bd54393c5f29
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 12:18:37 +0900

    Complete Section3 Step1 - create an algorithm for badges

---
+++ GratefulMoments/Models/BadgeManager.swift
@@ -0,0 +1,10 @@
+import Foundation
+import SwiftData
+
+class BadgeManager {
+    private let modelContainer: ModelContainer
+
+    init(modelContainer: ModelContainer) {
+        self.modelContainer = modelContainer
+    }
+}
