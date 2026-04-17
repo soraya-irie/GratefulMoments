516421f4354b07b406a6db18dbe346c7ab704369
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 15:04:10 +0900

    Complete Section5 Step2 - create an algorithm for badges

--- GratefulMoments/Tabs/Achievements/AchievementsView.swift
+++ GratefulMoments/Tabs/Achievements/AchievementsView.swift
@@ -2,10 +2,19 @@ import SwiftUI
 
 struct AchievementsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        contentStack
+    }
+
+    private var contentStack: some View {
+        VStack {
+            Text("Your Badges")
+            Text("Locked Badges")
+        }
+        .padding()
     }
 }
 
 #Preview {
     AchievementsView()
+        .sampleDataContainer()
 }
