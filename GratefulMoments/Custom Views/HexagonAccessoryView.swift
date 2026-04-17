1ff0fa486f157fe4b59cbc8db3533b526607603d
soraya-ire <rujiangkongshi@gmail.com>
2026-04-17 19:51:16 +0900

    Complete Section7 Step3 - create an algorithm for badges

---
+++ GratefulMoments/Custom Views/HexagonAccessoryView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct HexagonAccessoryView: View {
+    let moment: Moment
+    let hexagonLayout: HexagonLayout
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MomentHexagonView(moment: .sample, layout: .large)
+}
