cd0a94a6b78d144337d9f4c6a2e5b12581fa2e43
soraya-ire <rujiangkongshi@gmail.com>
2026-04-20 15:29:30 +0900

    Complete Section1 Step3 - investigate and fix a bug

--- GratefulMomentsTests/StreakCalculatorTests.swift
+++ GratefulMomentsTests/StreakCalculatorTests.swift
@@ -1,6 +1,10 @@
 import Testing
+@testable import GratefulMoments
+import Foundation
 
 struct StreakCalculatorTests {
+    let streakCalculator = StreakCalculator()
+    let now = Date.now
 
     @Test func testCalculations() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
