f379713ea363e02d62aedecf4afc593ade14f006
soraya-ire <rujiangkongshi@gmail.com>
2026-04-20 13:33:03 +0900

    Complete Section2 Step4 - add inclusive features

--- GratefulMoments/Custom Views/HexagonAccessoryView.swift
+++ GratefulMoments/Custom Views/HexagonAccessoryView.swift
@@ -21,6 +21,7 @@ struct HexagonAccessoryView: View {
             if badges.count > 1 {
                 Text("+\(badges.count)")
                     .bold()
+                    .minimumScaleFactor(0.3)
                     .frame(width: size * 0.5, height: size * 0.5)
                     .padding(8)
                     .background {
