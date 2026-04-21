f8e5de2d1b3df2b15e26457561b9d6d8adbe7733
soraya-ire <rujiangkongshi@gmail.com>
2026-04-21 10:54:15 +0900

    Complete Section1 Step5 - investigate and fix a bug

--- GratefulMomentsTests/StreakCalculatorTests.swift
+++ GratefulMomentsTests/StreakCalculatorTests.swift
@@ -10,7 +10,10 @@ struct StreakCalculatorTests {
         let days = [-2, -1]
         let expectedStreak = 2
 
-        let moments = [Moment]()
+        let moments = days.map {
+            let date = Calendar.current.date(byAdding: .day, value: $0, to: .now)!
+            return Moment(title: "", note: "", timestamp: date)
+        }
 
         let streak = streakCalculator.calculateStreak(for: moments)
         #expect(streak == expectedStreak)
